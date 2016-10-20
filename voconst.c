f98974aad2163169809aff2e0181897fd9614fad
ParimalaDhandapani <parimala7273@gmail.com>
2016-10-20 11:34:44 +0530

    Create voconst.c

---
+++ voconst.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char r=b;
+if(r=='a'||r=='e'||r=='i||r=='o'||r=='u')
+{
+printf("character is vowel");
+}else
+{
+printf("character is consonant");
+}
+getch();
+}
